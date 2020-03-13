33459ba333f60b9ac21998a4fd8fec22e393f6f3
erick rennan coelho <ercosc@gmail.com>
2020-03-13 20:11:19 -0300

    inserindo app portfolio

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio.apps.PortfolioConfig',
     'book.apps.BookConfig',
     'django.contrib.admin',
     'django.contrib.auth',
